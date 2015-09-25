037a9dd94ace22f9511969ebfaa625fb79a58218
Nikola Hardi <atlantic777@lugons.org>
2015-09-25 23:58:49 +0200

    Implemented getGravityDistances

--- src/gpulp/gravity_utils.cpp
+++ src/gpulp/gravity_utils.cpp
@@ -1,4 +1,5 @@
 #include "gpulp/gravity_utils.h"
+#include <cmath>
 
 using namespace gpulp;
 
@@ -16,6 +17,11 @@ std::vector<int> gpulp::arg_sort_pixels(InterpolationContext &ctx) {
 
 std::vector<float> gpulp::get_gravity_distances(InterpolationContext &ctx) {
   std::vector<float> dst;
+
+  dst.push_back(pow(ctx.dx, 2) + pow(ctx.dy, 2));
+  dst.push_back(pow(1-ctx.dx, 2) + pow(ctx.dy, 2));
+  dst.push_back(pow(ctx.dx, 2) + pow(1-ctx.dy, 2));
+  dst.push_back(pow(1-ctx.dx, 2) + pow(1-ctx.dy, 2));
 
   return dst;
 }
--- test/gpulp/test_gravity.cpp
+++ test/gpulp/test_gravity.cpp
@@ -11,7 +11,12 @@ TEST(GravityFloat, GetGravityDistances) {
 
   std::vector<float> dst = get_gravity_distances(ctx);
 
-  FAIL() << "finish test";
+  ASSERT_EQ(4, dst.size());
+
+  ASSERT_FLOAT_EQ(0.04 + 0.16, dst[0]); // top left
+  ASSERT_FLOAT_EQ(0.64 + 0.16, dst[1]); // top right
+  ASSERT_FLOAT_EQ(0.04 + 0.36, dst[2]); // bottom left
+  ASSERT_FLOAT_EQ(0.64 + 0.36, dst[3]); // bottom right
 }
 
 TEST(GravityFloat, SortPixels) {
